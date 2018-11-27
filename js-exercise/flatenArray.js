25991c6a5a2322178b5650dcd870f03c891b6cbb
sadhanagit <khadkasdhana@gmail.com>
2018-11-27 19:44:58 +0545

    updated

--- js-exercise/flatenArray.js
+++ js-exercise/flatenArray.js
@@ -1,6 +1,36 @@
-array = [[1, 2, 3], [2, 4, 5], 6];
+var input = [
+  [1, 2, 3],
+  [2, 4, 5], 6, {
+    name: 'john'
+  }
+];
+var output = [];
 
-var result = array.reduce(function (total, amount) {
-  return total.concat(amount);
-}, []);
-console.log(result);
+function flatArr(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      // console.log('true');
+      flatArr(array[i]);
+    } else {
+
+
+      if (addToArray(array[i], output)) {
+        output.push(array[i]);
+      }
+      // output.push(array[i]);
+    }
+  }
+}
+
+function addToArray(input, arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === input) {
+      console.log('false');
+      return false;
+    }
+  }
+  return true;
+}
+flatArr(input);
+console.log("output");
+console.log(output);
